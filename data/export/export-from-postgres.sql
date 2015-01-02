1c3b502123da592b5a46fd2c7f9ae1cb6e9fd530
Mark Needham <mark.needham@neotechnology.com>
2015-01-02 14:36:16 +0000

    adding other parts of the graph

--- data/export/export-from-postgres.sql
+++ data/export/export-from-postgres.sql
@@ -3,7 +3,9 @@ COPY (SELECT * FROM suppliers) TO '/tmp/suppliers.csv' WITH CSV header;
 COPY (SELECT * FROM products)  TO '/tmp/products.csv' WITH CSV header;
 COPY (SELECT * FROM employees) TO '/tmp/employees.csv' WITH CSV header;
 COPY (SELECT * FROM categories) TO '/tmp/categories.csv' WITH CSV header;
-
+COPY (SELECT * FROM region) TO '/tmp/regions.csv' WITH CSV header;
+COPY (SELECT * FROM territories) TO '/tmp/territories.csv' WITH CSV header;
+COPY (SELECT * FROM employeeterritories) TO '/tmp/employeeterritories.csv' WITH CSV header;
 COPY (SELECT * FROM orders
-  LEFT OUTER JOIN order_details ON order_details."OrderID" = orders."OrderID") TO '/tmp/orders.csv' WITH CSV header;
-  
+      LEFT OUTER JOIN order_details ON order_details."OrderID" = orders."OrderID")
+TO '/tmp/orders.csv' WITH CSV header;
